c4daff49bc142a300c0c417237e8bdbe3e72f53d
mschenk7474 <masonschenk@gmail.com>
2023-10-07 12:48:11 -0600

    piece done

--- testBoard.h
+++ testBoard.h
@@ -22,44 +22,44 @@ public:
         // Reset
         testReset();
         
-//        // SWAP
-//        testSwapKingRook();
-//        testSwapPawnSpace();
-//        testSwapBlackPieceWhitePiece();
-//
-//        // -=
-//        testDeletePieceWhite();
-//        testDeletePieceBlack();
-//
-//        // GET PIECE
-//        testGetPieceWhite();
-//        testGetPieceBlack();
-//
-//        // TURN
-//        testWhiteTurnTrue();
-//        testWhiteTurnFalse();
-//
-//        // CURRENT MOVE
-//        testGetCurrentMove();
+        // SWAP
+        testSwapKingRook();
+        testSwapPawnSpace();
+        testSwapBlackPieceWhitePiece();
+
+        // -=
+        testDeletePieceWhite();
+        testDeletePieceBlack();
+
+        // GET PIECE
+        testGetPieceWhite();
+        testGetPieceBlack();
+
+        // TURN
+        testWhiteTurnTrue();
+        testWhiteTurnFalse();
+
+        // CURRENT MOVE
+        testGetCurrentMove();
        
-//       // MOVE
-//       void move_pawnSimple() const;
-//       void move_pawnCapture() const;
-//       void move_pawnDouble() const;
-//       void move_pawnEnpassant() const;
-//       void move_pawnPromotion() const;
-//       void move_rookSlide() const;
-//       void move_rookAttack() const;
-//       void move_bishopSlide() const;
-//       void move_bishopAttack() const;
-//       void move_knightMove() const;
-//       void move_knightAttack() const;
-//       void move_queenSlide() const;
-//       void move_queenAttack() const;
-//       void move_kingMove() const;
-//       void move_kingAttack() const;
-//       void move_kingShortCastle() const;
-//       void move_kingLongCastle() const;
+       // MOVE
+        move_pawnSimple() ;
+        move_pawnCapture() ;
+        move_pawnDouble() ;
+        move_pawnEnpassant() ;
+        move_pawnPromotion() ;
+        move_rookSlide() ;
+        move_rookAttack() ;
+        move_bishopSlide() ;
+        move_bishopAttack() ;
+        move_knightMove() ;
+        move_knightAttack() ;
+        move_queenSlide() ;
+        move_queenAttack() ;
+        move_kingMove() ;
+        move_kingAttack() ;
+        move_kingShortCastle() ;
+        move_kingLongCastle() ;
 
        std::cout << "Passed Board Test Cases With Flying Colors!" << std::endl;
     }
